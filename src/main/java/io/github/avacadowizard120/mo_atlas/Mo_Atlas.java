daa4db6bb7984ea4f5f8df52772c5998ddf619d2
AvacadoWizard120 <edenjudeurias@gmail.com>
2024-05-30 13:16:27 -0500

    initial commit

---
+++ src/main/java/io/github/avacadowizard120/mo_atlas/Mo_Atlas.java
@@ -0,0 +1,13 @@
+package io.github.avacadowizard120.mo_atlas;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mo_Atlas implements ModInitializer
+{
+
+    @Override
+    public void onInitialize()
+    {
+
+    }
+}
---
+++ src/main/java/io/github/avacadowizard120/mo_atlas/client/Mo_AtlasClient.java
@@ -0,0 +1,12 @@
+package io.github.avacadowizard120.mo_atlas.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Mo_AtlasClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
